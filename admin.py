6dcbf7e36f7fb177eeb70bdcd817852ed846729b
aggelos <sinmaniphel@free.fr>
2017-12-24 10:54:19 +0100

    Added version management to admin

--- admin.py
+++ admin.py
@@ -1,15 +1,38 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
 from .model.story import Gender, Character, Location, Artifact, Scene, TimeFrame, Idea, Content, Story, SceneCharacter
 
-admin.site.register(TimeFrame)
+@admin.register(TimeFrame)
+class TimeFrameAdmin(VersionAdmin):
+    pass
+
+@admin.register(Character)
+class CharacterAdmin(VersionAdmin):
+    pass
+
+@admin.register(Location)
+class LocationAdmin(VersionAdmin):
+    pass
+
+@admin.register(Artifact)
+class ArtifactAdmin(VersionAdmin):
+    pass
+
+@admin.register(Scene)
+class SceneAdmin(VersionAdmin):
+    pass
+
+@admin.register(Idea)
+class IdeaAdmin(VersionAdmin):
+    pass
+
+@admin.register(Story)
+class StoryAdmin(VersionAdmin):
+    pass
+
+@admin.register(SceneCharacter)
+class SceneCharacterAdmin(VersionAdmin):
+    pass
+
 admin.site.register(Gender)
-admin.site.register(Character)
-admin.site.register(Location)
-admin.site.register(Artifact)
-admin.site.register(Scene)
-admin.site.register(Idea)
-admin.site.register(Content)
-admin.site.register(Story)
-admin.site.register(SceneCharacter)
-# Register your models here.
